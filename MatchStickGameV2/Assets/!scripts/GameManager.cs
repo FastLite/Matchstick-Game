73975d25e524c25c8ca19d87e98d40b5c9a5d8dc
iCSaH <t-rod@live.ca>
2022-02-01 10:20:20 -0500

    Updated

--- MatchStickGameV2/Assets/!scripts/GameManager.cs
+++ MatchStickGameV2/Assets/!scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     public bool gameIsEnded = false;
     public Slider sensativity;
     public Slider volume;
-   
+    
 
     private void Awake()
     {
