e7ec64d2c29a9ba82075b547b1aac55e3d8d1657
Vlad Volodin <Volodin_vlad2002@mail.ru>
2022-02-17 15:31:24 -0400

    Check commented code in interact script

---
+++ MatchStickGameV2/Assets/!scripts/Character/Interact.cs
@@ -0,0 +1,168 @@
+using CMF;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    //Two of the torch
+    //For now only torch defines if the player can light up other objects
+    public enum MyEnum
+    {
+        WithTorch,
+        Without
+    }
+
+    [Header("General")] [SerializeField]
+    private AdvancedWalkerController
+        controller; //We need this reference to restrict axis movement while dragging or pushing
+
+    private UIManager ui; //Referemce to the ui manager to use methods declared there
+
+    [Header("Raycast")] public Transform rayPoint; //From where ray is being cast forward
+
+    private TurnTowardControllerVelocity
+        TTCV; //Reference to the controller that is responsible for turning player model
+
+    [SerializeField] private float rayDistance = .5f; //pretty  self explanatory 
+
+    [Header("Dragging")] public Transform playerParent; //Object which will be the parent of the box and drag it along
+    private Transform currentBox; //Reference to the box to attach and detach the correct box
+
+    [Header("Torch")] public MyEnum state;
+    public GameObject torchGO; //torch gameobject, for visual representation and FireAbove check
+
+
+    private void Awake()
+    {
+        TTCV =
+            GetComponentInChildren<TurnTowardControllerVelocity
+            >(); //TurnTowardControllerVelocity is located on the child object
+
+        //If we didn't set the reference manually script will grab it from the current gameObject
+        if (controller != null)
+            return;
+        controller = gameObject.GetComponent<AdvancedWalkerController>();
+    }
+
+    private void Start()
+    {
+        //I get instance in the start() because in awake this script can load first
+        //and it won't be able to find reference anymore
+        ui = UIManager.instance;
+    }
+
+    void Update()
+    {
+        //Cast ray from the raypoint transform and draw it in scene mode as debug
+        //Distance for the actual ray will be different than the one for the debug
+        RaycastHit hit;
+        Debug.DrawRay(rayPoint.transform.position, rayPoint.forward, Color.green);
+        if (Physics.Raycast(rayPoint.position, rayPoint.TransformDirection(Vector3.forward), out hit, rayDistance))
+        {
+            //for easier access git gameobject is stored on local variable
+            GameObject go = hit.collider.gameObject;
+
+            //since we might compare tag later on it is also stored in local variable as a string
+            var tag = go.tag;
+            switch (tag)
+            {
+                //if it wasn't interactable hide ui tip just in case
+                //TODO find where to replace repeated GetButton call
+                default:
+                    ui.HideTip();
+                    break;
+                case "Box":
+                    ui.ShowTip();
+                    //When player holds space the box is attached to the parent transform
+                    if (Input.GetButtonDown("Jump"))
+                        PickUpBox(go.transform);
+                    //When player releases the button box becomes immovable again and detaches from the player
+                    if (Input.GetButtonUp("Jump"))
+                        ReleaseBox();
+                    break;
+
+                case "Button":
+                    ui.ShowTip();
+                    if (Input.GetButtonDown("Jump"))
+                        go.GetComponent<enterButton>().Activate();
+                    break;
+
+                case "TorchPile":
+                    ui.ShowTip();
+                    if (Input.GetButtonDown("Jump"))
+                        GrabTorch();
+                    break;
+
+                case "SSButton":
+                    if (Input.GetButtonDown("Jump"))
+                        go.GetComponent<enterButton>().StartSimonSaysGame();
+                    break;
+
+                case "SSBoxes":
+                    if (Input.GetButtonDown("Jump"))
+                        go.GetComponent<enterButton>().ChangeColor();
+                    break;
+
+                case "FirePlace":
+                    ui.ShowTip();
+                    //If player doesn't have torch - do nothing
+                    if (state == MyEnum.Without)
+                        return;
+                    //if he has - light up fireplace
+                    if (Input.GetButtonDown("Jump"))
+                        go.GetComponent<Fire>().InteractWithFire();
+                    break;
+            }
+        }
+        else
+        {
+            //Raycast hit nothing? Hide tip and do nothing
+            ui.HideTip();
+        }
+    }
+
+    void PickUpBox(Transform box)
+    {   //get the box reference from the object that ray hit an zero out it's position
+        currentBox = box;
+        currentBox.position.Set(0, 0, 0);
+        //set parent to the object we specified
+        currentBox.parent = playerParent;
+        //check the player facing direction and based on that restrict movement on one of the axis
+        int y = Mathf.FloorToInt(playerParent.transform.eulerAngles.y); 
+        TTCV.enabled = false;
+        if ((y < 95 && y > 85) || (y > 265 && y < 280))
+        {
+            controller.ignoreHorizontal = true;
+        }
+        else if ((y > -1 && y < 2) || (y > 175 && y < 185))
+        {
+            controller.ignoreVertical = true;
+        }
+    }
+
+    
+    //turn of everything we can to reset th box and controller states
+    private void ReleaseBox()
+    {
+        if (currentBox == null)
+        {
+            return;
+        }
+
+        GetComponentInChildren<TurnTowardControllerVelocity>().enabled = true;
+        controller.ignoreHorizontal = false;
+        controller.ignoreVertical = false;
+        currentBox.parent = null;
+        currentBox = null;
+    }
+
+    
+    //Change player torch state and activate torch gameobject
+    void GrabTorch()
+    {
+        if (state == MyEnum.WithTorch)
+            return;
+        state = MyEnum.WithTorch;
+        torchGO.SetActive(true);
+        torchGO.GetComponent<IgniteFireAbove>().enabled = true;
+    }
+}
