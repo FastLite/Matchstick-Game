846242e3de2c4598c2166c08635c305454b32c4d
TaureanR <73858746+TaureanR@users.noreply.github.com>
2022-04-05 17:38:00 -0400

    Fixed animation clipping
    Model would clip and fall through floor
    Froze root y transformation

--- MatchStickGameV2/Assets/!scripts/Character/Interact.cs
+++ MatchStickGameV2/Assets/!scripts/Character/Interact.cs
@@ -119,7 +119,7 @@ public class Interact : MonoBehaviour
         else
         {
             //Raycast hit nothing? Hide tip and do nothing
-            ui.HideTip();
+            //ui.HideTip();
             ReleaseBox();
         }
     }
